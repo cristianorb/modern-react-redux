e3d11f611debcc2e1f4a7814013dc2c2c74aebc9
Cristiano Barreto <rcristianob@gmail.com>
2019-05-13 23:15:02 -0300

    Building content with jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click me!';
+}
+
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div>
+            <label className="name" htmlFor="name">
+                Enter a name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {getButtonText()}
+            </button>
+        </div>    
+    );
 };
 
 // Take the react component and show it on the screen
