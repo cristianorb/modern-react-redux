03e806d41bbac18515d0ad88328f2c2aece8964c
Cristiano Barreto <cristiano.barreto@softplan.com.br>
2019-05-17 13:04:33 -0300

    State in react components and lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    render() {
+
+    state = { lat: null, errorMessage: '' };        
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)
-        );
-        return (            
-            <div>Latitude: </div>
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: { this.state.errorMessage }</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={ this.state.lat } />
+        }
+        
+        return <Spinner message='Please accept location request' />
+    }
+    
+    render() {
+        return (
+            <div>
+                { this.renderContent() }
+            </div>
+        );
+    }
+    
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
