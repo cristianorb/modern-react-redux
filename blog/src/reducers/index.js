dacd60ae66424d693ea283d00334ecd3f58c7951
Cristiano Barreto <cristiano.barreto@softplan.com.br>
2019-05-30 13:30:14 -0300

    Redux store design

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
-    replaceMe: () => 'replaceMe'
+    posts: postsReducer,
+    users: usersReducer
 });
